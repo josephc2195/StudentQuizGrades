2f8684e07536907c6bd7f4643f83f73e426389e1
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-06 10:01:17 -0400

    added comments

--- src/main/java/com/mycompany/studentquizgrades/StudentQuizGrades.java
+++ src/main/java/com/mycompany/studentquizgrades/StudentQuizGrades.java
@@ -8,33 +8,30 @@ package com.mycompany.studentquizgrades;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-​
 /**
- *
+ * Program creates a map of grades by students from user input data. The highest and lowest grades are saved and shown to the user
  * @author chica
  */
 public class StudentQuizGrades {
-​
     public static void main(String[] args) {
-        Map<String, ArrayList<Integer>> grades = new HashMap<>();
-        Map<String, Double> avg = new HashMap<>();
-​
+        Map<String, ArrayList<Integer>> grades = new HashMap<>();//holds the student name, and list of students grades
+
+        //using userio class for any printing/getting input from user
         UserIO io = new UserIOConsoleImpl();
-        int numOfQuizzes = io.readInt("Please enter the amount of quizzes taken:", 0, 10);
-        int numOfStudents = io.readInt("How many students are you inputing?", 0, 10);
+        int numOfQuizzes = io.readInt("Please enter the amount of quizzes taken:", 0, 10);//quizzes are between 1-10
+        int numOfStudents = io.readInt("How many students are you inputing?", 0, 10);//students are between 1-10
         int high = 0, low = 0, quizTotal=0;
         String nameLow = null, nameHigh = null;
-        for (int i = 0; i < numOfStudents; i++) {
+        for (int i = 0; i < numOfStudents; i++) {//looping for each student
             String name = io.readString("Enter name of student: ");
-            ArrayList<Integer> stud = new ArrayList<>();
-            int studentQuizTotal = 0;
+            ArrayList<Integer> stud = new ArrayList<>();//arraylist used to keep track of all students grades
             for (int j = 0; j < numOfQuizzes; j++) {
-                stud.add(io.readInt("Enter grade for test " + (j + 1), 0, 100));
-                grades.put(name, stud);
+                stud.add(io.readInt("Enter grade for test " + (j + 1), 0, 100));//adding grades to arraylist declared previously
             }
+            grades.put(name, stud);//adding student name and arraylist of grades to first hashmap
+            //looping thru each student grade to find lowest and highest grades as well as recording which student did it. 
             for (int g : stud) {
                 quizTotal += g;
-                studentQuizTotal += g;
                 if (low == 0 || low > g) {
                     low = g;
                     nameLow = name;
@@ -44,14 +41,11 @@ public class StudentQuizGrades {
                     nameHigh = name;
                 }
             }
-​
-            avg.put(name, Double.valueOf(studentQuizTotal / (numOfQuizzes * numOfStudents)));
         }
-        double classAvg = quizTotal / (numOfStudents * numOfQuizzes);
+        double classAvg = quizTotal / (numOfStudents * numOfQuizzes); //getting the class average
+       
         io.print("Class average: " + classAvg);
         io.print("High grade of: " + high + " by " + nameHigh);
         io.print("Low grade of: " + low + " by " + nameLow);
-​
     }
-​
 }
